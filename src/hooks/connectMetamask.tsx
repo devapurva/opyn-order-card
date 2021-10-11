ba26c50af36e1dcc39843d28dd1f6a43c8362b6e
Apurva Wadekar <apurva.w17@gmail.com>
2021-10-11 23:01:19 +0530

    reset function, removed unwanted imports

--- src/hooks/connectMetamask.tsx
+++ src/hooks/connectMetamask.tsx
@@ -10,7 +10,11 @@ const useConnectMetamask = () => {
   const [accounts, setAccounts] = useState<string[]>([]);
   const [accountBalance, setAccountBalance] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
-  const [details, setDetails] = useState<ICardDetails>(cardDetails);
+  const [details, setDetails] = useState<ICardDetails>({ ...cardDetails });
+  const [spotClass, setSpotClass] = useState<string>("plus");
+  const [currentStep, setCurrentStep] = useState<ITXACTION>({
+    ...cardDetails.txAction?.[0],
+  });
 
   useEffect(() => {
     if (accounts?.length > 0 && !accountBalance) {
@@ -18,6 +22,53 @@ const useConnectMetamask = () => {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [accounts, accountBalance]);
+
+  const resetStates = () => {
+    const resetCard = { ...cardDetails };
+    resetCard.txAction[1].active = 1;
+    resetCard.txAction[4].active = 0;
+    setDetails(resetCard);
+    setCurrentStep(resetCard.txAction[0]);
+    setSpotClass("plus");
+    setAccounts([]);
+    setAccountBalance(null);
+  };
+
+  const incrementSteps = () => {
+    if (currentStep.id === 4) {
+      if (details?.position?.current === 0) {
+        return toast.error("oTokens cannot be Zero", { autoClose: 5000 });
+      } else if (details?.position?.current > details?.position?.total) {
+        return toast.error("oTokens cannot be more than total balance", {
+          autoClose: 5000,
+        });
+      }
+    }
+    if (currentStep.id === 5) {
+      toast.success("Trade Submitted", { autoClose: 2000 });
+      setTimeout(() => {
+        toast.info("Generating new order card..", { autoClose: 5000 });
+        resetStates();
+      }, 3000);
+    }
+
+    setLoading(true);
+    const txActionTemp = [...details.txAction];
+    const activeStep = (item: ITXACTION) => item.active === 1;
+    const activeIndex = txActionTemp.findIndex(activeStep);
+    if (activeIndex + 1 < txActionTemp.length) {
+      txActionTemp[activeIndex].active = 0;
+      txActionTemp[activeIndex + 1].active = 1;
+      setCurrentStep(txActionTemp[activeIndex + 1]);
+    }
+    setTimeout(() => {
+      setDetails({
+        ...details,
+        txAction: txActionTemp,
+      });
+      setLoading(false);
+    }, 500);
+  };
 
   const setMax = () => {
     setDetails({
@@ -36,6 +87,7 @@ const useConnectMetamask = () => {
       await web3.eth
         .requestAccounts()
         .then((response: Array<string>) => {
+          setCurrentStep(cardDetails.txAction?.[1]);
           toast.success("Connected to Metamask!", {
             autoClose: 5000,
           });
@@ -91,34 +143,16 @@ const useConnectMetamask = () => {
     }
   };
 
-  const setCurrent = (value = "0") => {
-    // eslint-disable-next-line use-isnan
-    if (parseFloat(value) !== NaN) {
-      setDetails({
-        ...details,
-        position: {
-          ...details.position,
-          current: parseFloat(value),
-        },
-      });
-    } else {
-      toast.error("Position Size must be a number");
-    }
-  };
-
   return {
-    accounts,
-    accountBalance,
     loading,
     details,
-    setAccounts,
-    setAccountBalance,
-    setLoading,
+    spotClass,
+    currentStep,
     setDetails,
     ethEnabled,
-    getBalance,
     setMax,
-    setCurrent,
+    setSpotClass,
+    incrementSteps,
   };
 };
 
